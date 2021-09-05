dcb658d673f15c965de32a8a599573f7dc874df4
Peter-Rambeck <peter.rambeck@me.com>
2021-09-05 22:12:27 +0200

    almost

--- modules/utils.py
+++ modules/utils.py
@@ -2,13 +2,14 @@
 import argparse
 import csv
 import os
+import sys
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='A program that downloads a URL and stores it locally')
-    parser.add_argument('path', help='The URL to process')
-    parser.add_argument('lst', help='The name of the file to store the url in')
+#if __name__ == '__main__':
+#    parser = argparse.ArgumentParser(description='A program that downloads a URL and stores it locally')
+#    parser.add_argument('path', help='The path to folder')
+#    parser.add_argument('filename', help='The name of the file to store the url in')
 
-    args = parser.parse_args()
+#    args = parser.parse_args()
     # print('URL:', args.url)
     # print('Destination:', args.destination)
 
@@ -18,5 +19,21 @@ def get_file_names(folderpath = 'tmp/', filename = 'tmp/output.txt'):
     filenames = os.listdir(folderpath)   
     with open(filename, 'w') as file:
         file.write(str(filenames))
+
+def get_all_file_names(folderpath = 'tmp/', filename = 'tmp/output2.txt'):
+    """ takes a path to a folder and writes all filenames in the folder to a specified output file""" 
+    r = []
+    for root, dirs, files in os.walk(folderpath):
+        for name in files:
+            r.append(os.path.join(root, name))
+   
+    with open(filename, 'w') as file: 
+        new_r = list(reversed(r))       
+        print("1" + str(r))
+        print("2" + str(new_r))
+        file.write(str(new_r))
+
+if __name__ == "__main__":
+    get_all_file_names(sys.argv[0], sys.argv[1])
 
     
